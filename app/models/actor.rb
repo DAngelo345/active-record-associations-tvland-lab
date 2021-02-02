d73a14adbd38c8bee134c1de695783064760c071
DAngelo Thomas <dangelo1thomas@gmail.com>
2021-02-02 14:34:50 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,13 @@
 class Actor < ActiveRecord::Base
-  
+    has_many :characters 
+    has_many :shows
+    
+
+    def full_name
+        name = self.first_name 
+        name2 = self.last_name
+        
+    end
+
+   
 end
